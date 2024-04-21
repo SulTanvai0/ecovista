b8c0c66d2966a56b70d0c8fce5ae3348ed5966dc
SulTanvai0 <MuhammedMehedih@gmail.com>
2024-04-21 06:00:38 +0600

    uploading ecovista app

---
+++ components/LocationSwitcher.jsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { getLocationLatLongList } from "@/lib/location-info";
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+const LocationSwitcher = () => {
+    const [showLocationList, setShowLocationList] = useState(false);
+    const [locations, setLocations] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+
+    useEffect(() => {
+        async function getLocationList() {
+
+            const locationList = await getLocationLatLongList();
+            setLocations(locationList);
+            setLoading(false)
+        }
+
+        getLocationList();
+    }, []);
+
+    return (
+        <div className="relative">
+            <button onClick={() =>
+                setShowLocationList(!showLocationList)
+
+            }>
+                <Image
+                    className="size-9"
+                    src="link.svg"
+                    alt="link icon"
+                    width={36}
+                    height={36}
+                />
+            </button>
+
+            {
+                showLocationList && (
+                    <div className="absolute left-0 top-12 z-[999] w-full min-w-[280px] rounded-md bg-white p-4 shadow max-md:-translate-x-1/2">
+
+                        <ul
+                            role="list"
+                            className="divide-y divide-gray-100 [&>*]:py-2 [&>li]:cursor-pointer"
+                        >
+                            {loading ? <li>Loading...</li> : locations.map((info) => (
+
+                                <li key={info.location} onClick={() => setLoading(true)}>
+                                    <Link
+                                        href={`/${info.location}?latitude=${info.latitude}&longitude=${info.longitude}`}
+                                    >
+                                        {info.location}
+                                    </Link>
+                                </li>
+                            ))}
+
+                        </ul>
+                    </div>
+                )
+            }
+        </div >
+    );
+};
+
+export default LocationSwitcher;
